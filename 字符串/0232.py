85843e1e58e046e2415621f6036bb87b513ed167
zhouweii234 <62086490+zhouweii234@users.noreply.github.com>
2022-02-09 13:26:49 +0800

    Create 0232.py

---
+++ 字符串/0232.py
@@ -0,0 +1,54 @@
+```python
+class MyQueue(object):
+
+    def __init__(self):
+        self.stack_in = []
+        self.stack_out = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack_in.append(x)
+
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        if self.empty():
+            return None
+
+        if self.stack_out:
+            return self.stack_out.pop()
+        else:
+            for i in range(len(self.stack_in)):
+                self.stack_out.append(self.stack_in.pop())
+            return self.stack_out.pop()
+
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        ans = self.pop()
+        self.stack_out.append(ans)
+        return ans
+
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return not(self.stack_in or self.stack_out)
+
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
+```
